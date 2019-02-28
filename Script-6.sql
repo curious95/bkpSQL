0f7b77a3efb9debb0e371153c2b2f4f4a5fb31c4
curious95 <kamalpradhan95@outlook.com>
2019-02-28 11:33:14 +0000

    Removed magento version problem

--- Script-6.sql
+++ Script-6.sql
@@ -44,7 +44,8 @@ SELECT COUNT(*) FROM [woocommerce.V1.raw];
 SELECT TOP(10) * FROM [woocommerce.V1.raw] where total_versions >= 2.0; 
 
 
-UPDATE [woocommerce.V1.raw] set versions  = REPLACE(cast(versions as varchar(8000)), '.csv', '');
+UPDATE [magento.V1.raw] set versions  = REPLACE(cast(versions as varchar(8000)), '.csv', '');
+
 
 CREATE TABLE [dbo].[woocommerce.V1.production]
 (
